53be16afd34fef568e866bc7912a09cf8176f0f3
sylvain <fraisse.sylvain@gmail.com>
2023-05-18 10:15:44 +0200

    :sparkles: domain Workflow and worker bootstrap
    A Workflow is just, for now, a function that could be run through a remote trigger.
    It is defined by a `name` to be referenced.
    A Worker allows to run a Workflow on a specific environment.
    The trigger that starts the workflow and the Worker that actually execute the specified workflow could be one difference processes. An EventBus is used to communicate between this 2 processes.
    In this first implementation, a simple in-memory eventbus is implemented in order to test the flow.

---
+++ test/domain/workflow.test.ts
@@ -0,0 +1,63 @@
+import * as assert from 'assert'
+import { beforeEach, describe, it } from 'node:test'
+
+import {
+  createWorker,
+  createWorkerProxy,
+  EventBus,
+  WorkerProxy,
+} from '@workflow-runner/domain/worker'
+import { workflow } from '@workflow-runner/domain/workflow'
+import { InMemoryEventBus } from '@workflow-runner/infrastructure/bus/in-memory-event-bus'
+
+describe('Given a workflow nested in a worker', () => {
+  const greet = async (name: string): Promise<string> => {
+    console.log('Execute the greet workflow', name)
+    return `Hello, ${name}!`
+  }
+
+  let eventBus: EventBus
+
+  beforeEach(() => {
+    eventBus = new InMemoryEventBus()
+    createWorker(eventBus)(workflow('Greet', greet))
+  })
+
+  describe('the worker proxy runner', () => {
+    let workerProxy: WorkerProxy
+
+    beforeEach(() => {
+      workerProxy = createWorkerProxy(eventBus)
+    })
+
+    it('should return the workflow result when running', async () => {
+      const workflowResult = await workerProxy.run({
+        workflowName: 'Greet',
+        workflowId: '123',
+        payload: 'Jane Doe',
+      })
+
+      assert.strictEqual(workflowResult, 'Hello, Jane Doe!')
+    })
+
+    it('should return the workflow started status when starting', async () => {
+      const isWorkflowStarted = await workerProxy.start({
+        workflowName: 'Greet',
+        workflowId: '123',
+        payload: 'Jane Doe',
+      })
+
+      assert.strictEqual(isWorkflowStarted, true)
+    })
+
+    it('should return the workflow NOT started result when starting without workflow', async () => {
+      const isWorkflowStarted = await workerProxy.start({
+        workflowName: 'UnregisteredWorkflowName',
+        workflowId: '123',
+        payload: 'Jane Doe',
+      })
+
+      assert.strictEqual(isWorkflowStarted, false)
+    })
+  })
+})
