57ae1b0bf92a7f8a5b4a6713b8c1adf15bafde96
sylvain <fraisse.sylvain@gmail.com>
2023-05-23 23:22:59 +0200

    :sparkles: WorkflowRepository implementation using mongodb
    An utility script is provided to easily start a docker container running a mongodb database server.
    It is possible to start it use-ing `.local/bin/run-db start` and stop it with `.local/bin/run-db stop`.
    The default configuration allows a connection through the `'mongodb://root:rootpass@0.0.0.0:27017'` URL.

---
+++ src/infrastructure/repository/mongo/workflow.ts
@@ -0,0 +1,62 @@
+import { Collection, Db, MongoClient } from 'mongodb'
+
+import { WorkflowRepository, WorkflowRuntime } from '@workflow-runner/domain/ports'
+
+class MongoWorkflowRepository implements WorkflowRepository {
+  #workflows: Collection<WorkflowRuntime>
+
+  constructor(db: Db) {
+    this.#workflows = db.collection<WorkflowRuntime>('workflows')
+  }
+
+  async save(workflowRuntime: WorkflowRuntime) {
+    const { matchedCount } = await this.#workflows.replaceOne(
+      { id: workflowRuntime.id },
+      workflowRuntime,
+    )
+    if (matchedCount === 0) {
+      await this.#workflows.insertOne(workflowRuntime)
+    }
+  }
+
+  find(workflowRuntimeId: string) {
+    return this.#workflows.findOne({ id: workflowRuntimeId })
+  }
+}
+
+export type Options = {
+  url: string
+  dbName?: string
+}
+
+export type RepositoryManager = {
+  forWorkflow: () => WorkflowRepository
+  dropAll: () => Promise<void>
+  close: () => Promise<void>
+}
+
+const workflowRepositoryManager = async ({
+  url,
+  dbName = 'temporalight',
+}: Options): Promise<RepositoryManager> => {
+  const client = new MongoClient(url, {
+    tls: false,
+    ssl: false,
+    connectTimeoutMS: 1000,
+  })
+  await client.connect()
+  const db = client.db(dbName)
+
+  return {
+    forWorkflow: () => new MongoWorkflowRepository(db),
+
+    close: () => client.close(),
+
+    dropAll: async () => {
+      const collections = await db.listCollections().toArray()
+      await Promise.all(collections.map((collection) => db.dropCollection(collection.name)))
+    },
+  }
+}
+
+export { workflowRepositoryManager }
