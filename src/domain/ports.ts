aafe85bfc33b43b1420078533ab34accc1a0f3d1
sylvain <fraisse.sylvain@gmail.com>
2023-05-23 22:40:03 +0200

    :label: WorkflowRepository port definition

--- src/domain/ports.ts
+++ src/domain/ports.ts
@@ -1,3 +1,5 @@
+import { Nullable } from '@workflow-runner/core/types'
+
 export type Listener = (...arg: any[]) => any
 
 export type EventBus = {
@@ -5,4 +7,17 @@ export type EventBus = {
   off: (eventName: string) => void
   send: <Event = unknown>(eventName: string, event: Event) => Promise<boolean>
   close: () => Promise<void>
+}
+
+export type RuntimeStatus = 'UNINITIALIZED' | 'STARTED' | 'SUCCEED' | 'FAILED'
+
+export type WorkflowRuntime = {
+  name: string
+  id: string
+  status: RuntimeStatus
+}
+
+export type WorkflowRepository = {
+  save: (workflowRuntime: WorkflowRuntime) => Promise<void>
+  find: (workflowRuntimeId: string) => Promise<Nullable<WorkflowRuntime>>
 }
